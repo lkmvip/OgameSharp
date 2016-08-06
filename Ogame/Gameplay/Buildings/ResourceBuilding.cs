4288d8dd9ba14464714cc7efc079c5e17daffb6b
ImSerious <deaman57100@hotmail.com>
2016-08-06 16:15:30 +0200

    [CHANGE] Minor fixes

---
+++ Ogame/Gameplay/Buildings/ResourceBuilding.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ogame.Gameplay.Buildings
+{
+    /// <summary>
+    /// A resource building, like a mine or a power plant.
+    /// </summary>
+    public class ResourceBuilding : Building
+    {
+        Resource production;
+        long metalStockage;
+        long crystalStockage;
+        long deuteriumStockage;
+    }
+}
